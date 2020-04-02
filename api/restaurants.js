ea468923d2d909317cb577bcccc818a98b68705a
Tzuyi Yang <a0988426059@gmail.com>
2020-04-02 15:06:09 +0800

    Add restaurantCard component

--- api/restaurants.js
+++ api/restaurants.js
@@ -1,6 +1,9 @@
 import { apiHelper } from '../plugins/axios'
 
 export default {
+  getRestaurants() {
+    return apiHelper.get('/restaurants')
+  },
   getFeeds() {
     return apiHelper.get('/restaurants/feeds')
   },
--- pages/restaurants/index.vue
+++ pages/restaurants/index.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">首頁 - 餐廳列表</h1>
+    <!-- 餐廳類別標籤 RestaurantsNavPills -->
+
+    <div class="row">
+      <!-- 餐廳卡片 RestaurantCard-->
+      <RestaurantCard
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+        :initial-restaurant="restaurant"
+      />
+    </div>
+
+    <!-- 分頁標籤 RestaurantPagination -->
   </div>
 </template>
 
 <script>
 import NavTabs from "../../components/NavTabs";
+import RestaurantCard from "../../components/RestaurantCard";
+import restaurantAPI from "../../api/restaurants";
 
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    RestaurantCard
+  },
+  data() {
+    return {
+      restaurants: [],
+      categories: [],
+      categoryId: -1,
+      currentPage: 1,
+      totalPage: -1
+    };
+  },
+  async asyncData() {
+    const { data } = await restaurantAPI.getRestaurants();
+
+    return {
+      restaurants: data.restaurants,
+      categories: data.categories,
+      categoryId: data.categoryId,
+      currentPage: data.page,
+      totalPage: data.totalPage.length
+    };
   }
 };
 </script>
